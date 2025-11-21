6aed9af4d28644e9edf6f2a0012d0b22bbd52c89
pshgrom <gromyko.pavel@lab-company.ru>
2025-11-21 10:34:42 +0300

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import { visualizer } from 'rollup-plugin-visualizer'
+import type { Config as SvgoConfig } from 'svgo'
 import { defineConfig } from 'vite'
 import viteCompression from 'vite-plugin-compression'
 import vuetify from 'vite-plugin-vuetify'
 import svgLoader from 'vite-svg-loader'
-import type { Config as SvgoConfig } from 'svgo'
 
 const isProd = process.env.NODE_ENV === 'production'
 const isAnalyze = process.env.ANALYZE === 'true'
@@ -16,14 +16,14 @@ const prodPlugins = [
     algorithm: 'gzip',
     ext: '.gz',
     threshold: 10240,
-    filter: /\.(js|mjs|json|css|html|svg)$/i
+    filter: /\.(js|mjs|json|css|html)$/i
   }),
 
   viteCompression({
     algorithm: 'brotliCompress',
     ext: '.br',
     threshold: 10240,
-    filter: /\.(js|mjs|json|css|html|svg)$/i,
+    filter: /\.(js|mjs|json|css|html)$/i,
     compressionOptions: { level: 11 }
   }),
 
@@ -42,8 +42,13 @@ const prodPlugins = [
 
 export default defineConfig({
   cacheDir: 'node_modules/.vite_cache',
+
   plugins: [
     vue({
+      script: {
+        defineModel: true,
+        propsDestructure: true
+      },
       template: {
         compilerOptions: {
           isCustomElement: (tag) => ['masked-input'].includes(tag)
@@ -63,8 +68,10 @@ export default defineConfig({
         ]
       } satisfies SvgoConfig
     }),
+
     ...(isProd ? prodPlugins : [])
   ],
+
   css: {
     preprocessorOptions: {
       scss: {
@@ -72,15 +79,18 @@ export default defineConfig({
       }
     }
   },
+
   server: {
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
     allowedHosts: ['slicertok-frontend', 'localhost']
   },
+
   build: {
     target: 'esnext',
     sourcemap: false,
+    cssMinify: 'lightningcss',
     cssCodeSplit: true,
     minify: 'esbuild',
     assetsInlineLimit: 2048,
@@ -92,22 +102,26 @@ export default defineConfig({
     rollupOptions: {
       output: {
         inlineDynamicImports: false,
+
         manualChunks(id) {
           if (id.includes('node_modules')) {
             if (id.includes('vuetify')) return 'vendor_vuetify'
             if (id.includes('vue')) return 'vendor_vue'
-            if (id.includes('axios')) return 'vendor_network'
+            if (id.includes('axios')) return 'vendor_axios'
             if (id.includes('vue-router')) return 'vendor_router'
             if (id.includes('pinia')) return 'vendor_pinia'
             return 'vendor_misc'
           }
+
           if (id.includes('src/views/')) {
             return 'view_' + id.split('src/views/')[1].split('/')[0]
           }
+
           if (id.includes('src/components/tables/')) return 'components_tables'
           if (id.includes('src/components/base/')) return 'components_base'
           if (id.includes('src/components/')) return 'components'
         },
+
         chunkFileNames: 'assets/[name]-[hash].js',
         entryFileNames: 'assets/[name]-[hash].js',
         assetFileNames: 'assets/[name]-[hash].[ext]'
@@ -120,14 +134,12 @@ export default defineConfig({
       'vue',
       'vue-router',
       'pinia',
-      'vue-the-mask',
       'axios',
-      'vuetify',
       'dompurify',
       'click-outside-vue3',
       'vue-qrcode'
     ],
-    exclude: []
+    exclude: ['vuetify']
   },
 
   resolve: {
@@ -138,6 +150,7 @@ export default defineConfig({
   },
 
   esbuild: {
+    target: 'esnext',
     drop: isProd ? ['console', 'debugger'] : [],
     legalComments: 'none'
   },
